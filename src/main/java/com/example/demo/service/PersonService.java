fd870fb31a0d48e6ffbc9dcca6e618d6c2577fd1
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2020-03-31 19:15:18 -0400

    fixed service annotation

--- src/main/java/com/example/demo/service/PersonService.java
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -11,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-
+//service
 @Service
 public class PersonService {
 	//insert a new person
